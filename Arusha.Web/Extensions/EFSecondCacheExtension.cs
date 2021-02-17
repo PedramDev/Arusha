96bba95e4b693e94489074aae1e97e96caa38b12
Pedram Karimi <PedramDev@hotmail.com>
2021-02-17 23:33:29 +0330

    cache added

---
+++ Arusha.Web/Extensions/EFSecondCacheExtension.cs
@@ -0,0 +1,28 @@
+﻿using CacheManager.Core;
+using EFCoreSecondLevelCacheInterceptor;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Newtonsoft.Json;
+
+namespace Arusha.Web.Extensions
+{
+    public static class EFSecondCacheExtension
+    {
+        public static void AddSecondCache(this IServiceCollection services)
+        {
+            services.AddEFSecondLevelCache(options =>
+            {
+                options.UseCacheManagerCoreProvider().DisableLogging(false);
+                options.CacheAllQueries(CacheExpirationMode.Absolute, TimeSpan.FromMinutes(30));
+            });
+
+            // Add an in-memory cache service provider
+            services.AddSingleton(typeof(ICacheManager<>), typeof(BaseCacheManager<>));
+            services.AddSingleton(typeof(ICacheManagerConfiguration),
+                new ConfigurationBuilder()
+                        .WithJsonSerializer()
+                        .WithMicrosoftMemoryCacheHandle(instanceName: "MemoryCache1")
+                        .Build());
+        }
+    }
+}
--- Arusha.Web/Startup.cs
+++ Arusha.Web/Startup.cs
@@ -1,3 +1,4 @@
+using Arusha.Web.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -23,6 +24,7 @@ namespace Arusha.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSecondCache();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
